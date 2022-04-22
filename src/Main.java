3b84e03d76b35a506806b435069e7300550ce908
denis <lego7777@mail.ru>
2022-04-22 14:04:29 +0300

    Add necessary exceptions.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.DataInputStream;
 import java.io.IOException;
 
 public class Main {
-    public static void main (String... args){
+    public static void main(String... args) throws Exception {
         {
             DataInputStream in = new DataInputStream(System.in);
 
@@ -15,58 +15,105 @@ public class Main {
             }
         }
     }
-    public static String calc(String input){
+
+    public static String calc(String input) throws Exception {
 
         int result = 0;
         String[] split = input.split(" ");
         int numberValueFirst;
         int numberValueSecond;
-        if (RomeNum.NONE.isRomeNumber(split[0])&RomeNum.NONE.isRomeNumber(split[2])){
+
+        if (split.length > 3) {
+            throw new Exception("throws Exception //т.к. ввод более двух операндов");
+        }
+
+        if (RomeNum.NONE.isRomeNumber(split[0]) & !RomeNum.NONE.isRomeNumber(split[2])) {
+            throw new Exception("throws Exception //т.к. ввод одновременно и римских и арабских чисел  ");
+        }
+        if (!RomeNum.NONE.isRomeNumber(split[0]) & RomeNum.NONE.isRomeNumber(split[2])) {
+            throw new Exception("throws Exception //т.к. ввод одновременно и римских и арабских чисел  ");
+        }
+
+        if (RomeNum.NONE.isRomeNumber(split[0]) & RomeNum.NONE.isRomeNumber(split[2])) {
             numberValueFirst = RomeNum.NONE.getArabicNumber(split[0]);
             numberValueSecond = RomeNum.NONE.getArabicNumber(split[2]);
-            if(split[1].equals("+")){
-                result = numberValueFirst+numberValueSecond;
-            }
-            else if (split[1].equals("-")){
-                result = numberValueFirst-numberValueSecond;
-            }
-            else if (split[1].equals("*")){
-                result = numberValueFirst*numberValueSecond;
-            }
-            else if (split[1].equals("/")){
-                result = numberValueFirst/numberValueSecond;
-            }
-            return countRomeResult(result);
-        }
-        else if (!RomeNum.NONE.isRomeNumber(split[0])&!RomeNum.NONE.isRomeNumber(split[2])){
-            numberValueFirst = Integer.valueOf(split[0]);
-            numberValueSecond = Integer.valueOf(split[2]);
 
-            if(split[1].equals("+")){
-                result = numberValueFirst+numberValueSecond;
+            if (numberValueFirst < 1 | numberValueFirst > 10) {
+                throw new Exception("throws Exception //т.к. допустимые числа ввода от 1 до 10 включительно");
             }
-            else if (split[1].equals("-")){
-                result = numberValueFirst-numberValueSecond;
-            }
-            else if (split[1].equals("*")){
 
-                result = numberValueFirst*numberValueSecond;
+            if (numberValueSecond < 1 | numberValueSecond > 10) {
+                throw new Exception("throws Exception //т.к. допустимые числа ввода от 1 до 10 включительно");
             }
-            else if (split[1].equals("/")){
-                result = numberValueFirst/numberValueSecond;
+            if (!ArifmeticAction.PLUS.isArifmeticsAction(split[1])) {
+                throw  new Exception("throws Exception //т.к. введен недопустимый операнд");
             }
+
+            if (split[1].equals("+")) {
+                result = numberValueFirst + numberValueSecond;
+            } else if (split[1].equals("-")) {
+                result = numberValueFirst - numberValueSecond;
+            } else if (split[1].equals("*")) {
+                result = numberValueFirst * numberValueSecond;
+            } else if (split[1].equals("/")) {
+                result = numberValueFirst / numberValueSecond;
+            }
+
+            String countResult = countRomeResult(result);
+
+            if (countResult.isEmpty()){
+                throw new Exception("throws Exception //т.к. римские цифры не могут быть отрицательными");
+            }
+
+            return countResult;
+        } else if (!RomeNum.NONE.isRomeNumber(split[0]) & !RomeNum.NONE.isRomeNumber(split[2])) {
+
+
+            try {
+                numberValueFirst = Integer.parseInt(split[0]);
+                numberValueSecond = Integer.parseInt(split[2]);
+            }catch (NumberFormatException e){
+
+                numberValueFirst = -1;
+                numberValueSecond = -1;
+            }
+
+            if (numberValueFirst < 1 | numberValueFirst > 10) {
+                throw new Exception("throws Exception //т.к. допустимые числа ввода от 1 до 10 включительно");
+            }
+
+            if (numberValueSecond < 1 | numberValueSecond > 10) {
+                throw new Exception("throws Exception //т.к. допустимые числа ввода от 1 до 10 включительно");
+            }
+            if (!ArifmeticAction.PLUS.isArifmeticsAction(split[1])) {
+                throw  new Exception("throws Exception //т.к. введен недопустимый операнд");
+            }
+
+            if (split[1].equals("+")) {
+                result = numberValueFirst + numberValueSecond;
+            } else if (split[1].equals("-")) {
+                result = numberValueFirst - numberValueSecond;
+            } else if (split[1].equals("*")) {
+
+                result = numberValueFirst * numberValueSecond;
+            } else if (split[1].equals("/")) {
+                result = numberValueFirst / numberValueSecond;
+            }
+
         }
+
 
         return String.valueOf(result);
 
     }
+
     public static String countRomeResult(int result) {
 
         String finalResult = "";
 
 
         if (result <= 10) {
-        finalResult = RomeNum.NONE.getRomeNumber(result);
+            finalResult = RomeNum.NONE.getRomeNumber(result);
         } else if (result < 50) {
             int countTen = result / 10;
             for (int q = 0; q < countTen; q++) {
@@ -75,27 +122,25 @@ public class Main {
             int secondNumber = result - (countTen * 10);
             finalResult = finalResult + "" + RomeNum.NONE.getRomeNumber(secondNumber);
             return finalResult;
-        }
-        else if (result==50){
+        } else if (result == 50) {
             finalResult = "L";
-        }
-        else if (result < 100) {
-            int countTen = result / 10;
+        } else if (result < 100) {
+            int countFive = result - 50;
+            int countTen = countFive / 10;
             finalResult = "L";
             for (int q = 0; q < countTen; q++) {
                 finalResult += "X";
-
             }
 
-            int secondNumber = result - (countTen * 10);
-            finalResult = finalResult + "" + secondNumber;
-        }
-        else if (result==100){
+            int secondNumber = result - (50+countTen*10);
+            finalResult = finalResult + "" + RomeNum.NONE.getRomeNumber(secondNumber);
+        } else if (result == 100) {
             finalResult = "C";
         }
 
         return finalResult;
     }
+
     enum RomeNum {
         NONE("0"),
         I("1"),
@@ -109,57 +154,62 @@ public class Main {
         IX("9"),
         X("10");
 
-            private String value;
+        private String value;
 
-            RomeNum(String v) {
-                value = v;
-            }
+        RomeNum(String v) {
+            value = v;
+        }
 
-            public boolean isRomeNumber(String input) {
-                for (RomeNum num : values()) {
-                    if (input.equals(num.name())) {
-                        return true;
-                    }
+        public boolean isRomeNumber(String input) {
+            for (RomeNum num : values()) {
+                if (input.equals(num.name())) {
+                    return true;
                 }
-                return false;
             }
+            return false;
+        }
 
-            int getArabicNumber(String romeNumber) {
-                for (RomeNum num : values()) {
-                    if (romeNumber.equals(num.name())) {
-                        return Integer.valueOf(num.value);
-                    }
+        int getArabicNumber(String romeNumber) {
+            for (RomeNum num : values()) {
+                if (romeNumber.equals(num.name())) {
+                    return Integer.valueOf(num.value);
                 }
-                return 0;
             }
+            return -1;
+        }
+
         String getRomeNumber(int romeNumber) {
             for (RomeNum num : values()) {
 
-                if (romeNumber!=0&&Integer.valueOf((num.value))==romeNumber) {
+                if (romeNumber != 0 && Integer.valueOf((num.value)) == romeNumber) {
                     return num.name();
                 }
             }
             return "";
         }
     }
-//    enum ArifmeticAction {
-//        PLUS("+"),
-//        MULTIPLY("*"),
-//        MINUS("-"),
-//        DIVIDE("/");
-//
-//        private String value;
-//
-//        ArifmeticAction(String a) {
-//            value = a;
-//        }
-//        String getArifmeticAction (String input ) {
-//            for (ArifmeticAction action : values()) {
-//                if (input.equals(action.value)){
-//                    return action.value;
-//                }
-//            }
-//            return "";
-//        }
-//    }
+
+    enum ArifmeticAction {
+        PLUS("+"),
+        MINUS("-"),
+        MULTY("*"),
+        DOUBLE("/");
+
+        private String value;
+
+        ArifmeticAction(String v) {
+            value = v;
+        }
+
+        public boolean isArifmeticsAction(String input) {
+            for (ArifmeticAction num : values()) {
+                if (input.equals(num.value)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
 }
+
